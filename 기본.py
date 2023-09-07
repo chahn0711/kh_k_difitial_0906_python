91ab0df31631778b8586b750ff0b748f082d2bb4
chahn0711 <chahn0711@naver.com>
2023-09-07 09:25:28 +0900

    2023.09.06

---
+++ 기본.py
@@ -0,0 +1,42 @@
+print("Hello, world")
+print('Hello, world')
+print(100)
+print(33.33)
+print(100+23)
+
+# 변수를 선언하고 값을 대입
+num = 100  # 데이터형은 값이 대입되는 순간에 결정 남
+print(num)
+num = "100"
+# 여기는 주석 입니다.
+"""
+여기는 주석 구간 입니다.
+마ㅓ이멍머아머ㅏ이머ㅣㄴ아ㅓㅁㄴ
+"""
+
+# 변수 활용
+name = "곰돌이사육사"
+email = "chahn0711@naver.com"
+age = 26
+addr = '서울시 강남구 역삼동 KH정보교육원'
+
+print(f"""
+이름 : {name}
+이메일 : {email}
+나이 : {age}
+주소 : {addr}
+""")
+
+# 파이썬은 불리언 값이 대문자로 시작
+isAdult = True
+
+if isAdult :
+    print("나는 성인 입니다.")
+else :
+    print("나는 성인이 아닙니다.")
+
+"""
+    작성자 : 곰돌이사육사
+    수정날짜 : 2023.09.06
+"""
+print([1, 2, 3]) # 리스트 출력
