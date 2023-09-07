863bf96293d197e1d9eef8f70747adf97e294ce7
chahn0711 <chahn0711@naver.com>
2023-09-07 17:20:33 +0900

    2023.09.07

--- 반복문.py
+++ 반복문.py
@@ -22,8 +22,110 @@
 #     print(e, end="/")
 
 # for 변수 in range(시작값, 종료값, 증감값):
-n = int(input("정수를 입력 하세요 : "))
-sum = 0
-for i in range(1, n+1): # 1부터 n+1 미만
-    sum += i
-print(sum)
+# n = int(input("정수를 입력 하세요 : "))
+# sum = 0
+# for i in range(1, n+1): # 1부터 n+1 미만
+#     sum += i
+# print(sum)
+
+# for문을 역순으로 반복하기
+# for i in range(10, -1, -1):  # 10 ~ 0 까지 출력
+#     print(i)
+
+# 이중 for문
+# n = int(input("정수를 입력 하세요 : "))
+# for i in range(0, n):
+#     for j in range(0, n):
+#         print("*", end=' ')
+#     print()
+
+# 구구단 찍기
+# for i in range(2, 10):  # 2단 ~ 9단
+#     for j in range(1, 10):
+#         print(f"{i} x {j} = {i*j}")
+#     print()
+
+# 홀수 / 짝수 나누어 찍기
+# n = int(input("정수 입력 : "))
+# for i in range(0, n):
+#     for j in range(0, n):
+#         if j % 2 == 0: print("$", end=' ')
+#         else: print("*", end=' ')
+#     print()
+
+# 사각형 찍기
+# 정수값 n을 입력 받아 n * n크기의 행렬을 출력하는 프로그램 작성
+# 값은 1부터 시작, 예쁘게 찍기
+# 3
+# 1 2 3
+# 4 5 6
+# 7 8 9
+# n = int(input("정수 입력 : "))
+# for i in range(1, n * n + 1):
+#     print(f"{i:>3}", end=' ')
+#     if i % n == 0: print()
+
+# 별 찍기1
+# *
+# * *
+# * * *
+# * * * *
+# n = int(input("별 갯수 입력 : "))
+# for i in range(n):
+#     for j in range(i+1):
+#         print("*", end=" ")
+#     print()
+
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
+# 내가 한거
+# n = int(input("별 갯수 입력 : "))
+# for i in range(n, 0, -1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+# n = int(input("뒤집어진 역삼각형 : "))
+# for i in range(n) :
+#     for j in range(n-i):
+#         print("*", end=" ")
+#     print()
+
+# * * * * *
+#   * * * *
+#     * * *
+#       * *
+#         *
+# n = int(input("뒤집어진 직각삼각형(반대) : "))
+# for i in range(n):
+#     for s in range(i):
+#         print(" ", end=" ")
+#     for j in range(n-i):
+#         print("*", end=" ")
+#     print()
+
+# * * * * *
+#  * * * *
+#   * * *
+#    * *
+#     *
+# n = int(input("역삼각형 : "))
+# for i in range(n, 0, -1):
+#     for s in range(n - i):
+#         print(" ", end="")
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+# 문자와 ASCII 코드
+# chr : 유니코드 값을 입력 받아 그 코드에 해당하는 문자를 출력
+# ord : 문자를 유니코드값으로 변환
+for i in range(ord("A"), ord("Z")+1):
+    print(chr(i), end=" ")
+print()
+
+for i in range(65, 91):
+    print(chr(i), end=" ")
+print()
