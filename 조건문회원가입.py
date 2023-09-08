b02c5330233fedf43353bf33e6c2bac053ebb626
chahn0711 <chahn0711@naver.com>
2023-09-08 09:12:59 +0900

    2023.09.07

--- 조건문회원가입.py
+++ 조건문회원가입.py
@@ -1,6 +1,6 @@
 # 회원 가입을 위한 아이디와 패스워드 입력 받기
 user = input("아이디 입력 : ")
-if len(user) >= 10:  # 입력 받은 아이디의 길이가 10과 같거나 커야함
+if len(user) >= 8:  # 입력 받은 아이디의 길이가 8과 같거나 커야함
     pw = input("비밀번호 입력 : ")
     # if pw.__len__() < 8 or pw.__len__() > 16:
     if len(pw) < 8 or len(pw) > 16:
@@ -8,7 +8,7 @@ if len(user) >= 10:  # 입력 받은 아이디의 길이가 10과 같거나 커�
     elif pw.find(user) >= 0:  # 없으면 -1을 반환 합니다.
         print("비밀번호에 아이디를 포함시킬 수 없습니다")
     else:
-       print("아이디 : {}".format(user))
-       print("비밀번호 : {}".format(pw))
+       print(f"아이디 : {user}")
+       print(f"패스워드 : {pw}")
 else:
-    print("아이디는 반드시 10자리 이상이여야합니다")
+    print("아이디는 반드시 자리 8이상이여야 합니다.")
