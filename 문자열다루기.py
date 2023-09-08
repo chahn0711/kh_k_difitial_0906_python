e817a7a2766ea67040af592376bde630a70b7313
chahn0711 <chahn0711@naver.com>
2023-09-08 09:00:05 +0900

    2023.09.06

--- 문자열다루기.py
+++ 문자열다루기.py
@@ -45,5 +45,27 @@ print(input_str2.__len__())  # 문자열에 포함된 내장 함수
 # 문자열 찾기 : find(), rfind(), index()
 # find() : 찾은 문자열의 첫번째 인덱스를 반환, 못 찾으면 -1을 반환
 # index() : 찾은 문자열의 첫번째 인덱스를 반환, 못 찾으면 ValueError 예외 발생
-phrase = "가장 큰 실수는 포기, 가장 어리석은"
+phrase = "가장 큰 실수는 포기, 가장 어리석은 일은 남의 결점 찾기, 가장 좋은 선물은 용서"
+print(phrase.find("가장"))
+print(phrase.rfind("가장"))   # 뒤에서 부터 찾지만 위치에 인덱스는 앞에서 부터 계산
+print(phrase.index("포기"))
 
+print(phrase.find("나에게"))   # 못 찾으면 -1
+# print(phrase.index("나에게"))   # 못 찾으면 value error
+
+new_phrase = phrase.replace("가장", "나에게")
+print(new_phrase)
+
+# 문자열 연산
+print("태양고" + "나희도")    # 가능
+# print("태양고" + 2)    # 문자열과 정수의 연산 불가능
+print("안녕하세요"*3)    # 해당 문자열을 수만큼 반복 수행
+print("안녕하세요", "!"*5, "\n", "\t", "="*3, "태양고", "="*3, "\n나희도"*3, "입니다.")
+
+# 문자열 앞/뒤 공백 제거 : strip()
+input_aa = """
+    안녕하세요.
+문자열 함수를 알아 봅시다.
+
+"""
+print(input_aa.strip())
